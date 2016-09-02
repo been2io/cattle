818fdfbc3cfe8533edb8ee479828bf09b57e9df9
ben <lifecontrol@live.cn>
2016-09-02 19:34:57 +0800

    scheduling base on active or starting and inactive instance

--- code/iaas/simple-allocator/src/main/java/io/cattle/platform/simple/allocator/dao/impl/SimpleAllocatorDaoImpl.java
+++ code/iaas/simple-allocator/src/main/java/io/cattle/platform/simple/allocator/dao/impl/SimpleAllocatorDaoImpl.java
@@ -4,9 +4,13 @@ import static io.cattle.platform.core.model.tables.AgentTable.*;
 import static io.cattle.platform.core.model.tables.HostTable.*;
 import static io.cattle.platform.core.model.tables.StoragePoolHostMapTable.*;
 import static io.cattle.platform.core.model.tables.StoragePoolTable.*;
+import static io.cattle.platform.core.model.tables.InstanceHostMapTable.*;
+import static io.cattle.platform.core.model.tables.InstanceTable.*;
+
 import io.cattle.platform.allocator.service.AllocationCandidate;
 import io.cattle.platform.archaius.util.ArchaiusUtil;
 import io.cattle.platform.core.constants.CommonStatesConstants;
+import io.cattle.platform.core.constants.InstanceConstants;
 import io.cattle.platform.db.jooq.dao.impl.AbstractJooqDao;
 import io.cattle.platform.object.ObjectManager;
 import io.cattle.platform.simple.allocator.AllocationCandidateCallback;
@@ -20,7 +24,12 @@ import javax.inject.Inject;
 
 import org.jooq.Condition;
 import org.jooq.Cursor;
+import org.jooq.Field;
+import org.jooq.Record;
+import org.jooq.Table;
 import org.jooq.Record2;
+import org.jooq.Record3;
+import org.jooq.Record4;
 import org.jooq.impl.DSL;
 
 import com.netflix.config.DynamicBooleanProperty;
@@ -40,26 +49,60 @@ public class SimpleAllocatorDaoImpl extends AbstractJooqDao implements SimpleAll
     public Iterator<AllocationCandidate> iteratorHosts(List<Long> volumes, QueryOptions options, AllocationCandidateCallback callback) {
         return iteratorHosts(volumes, options, true, callback);
     }
-
+    public static final Field<Integer> INSTANCE_NUM_PER_HOST=HOST.ID.count().as("instance_num");
     protected Iterator<AllocationCandidate> iteratorHosts(List<Long> volumes, QueryOptions options, boolean hosts,
             AllocationCandidateCallback callback) {
-        final Cursor<Record2<Long,Long>> cursor = create()
-                .select(HOST.ID, STORAGE_POOL.ID)
-                .from(HOST)
-                .leftOuterJoin(STORAGE_POOL_HOST_MAP)
-                    .on(STORAGE_POOL_HOST_MAP.HOST_ID.eq(HOST.ID)
-                        .and(STORAGE_POOL_HOST_MAP.REMOVED.isNull()))
-                .join(STORAGE_POOL)
-                    .on(STORAGE_POOL.ID.eq(STORAGE_POOL_HOST_MAP.STORAGE_POOL_ID))
-                .leftOuterJoin(AGENT)
-                    .on(AGENT.ID.eq(HOST.AGENT_ID))
-                .where(
-                    AGENT.ID.isNull().or(AGENT.STATE.eq(CommonStatesConstants.ACTIVE))
-                    .and(HOST.STATE.in(CommonStatesConstants.ACTIVE, CommonStatesConstants.UPDATING_ACTIVE))
-                    .and(STORAGE_POOL.STATE.eq(CommonStatesConstants.ACTIVE))
-                    .and(getQueryOptionCondition(options)))
-                .orderBy((SPREAD.get() ? HOST.COMPUTE_FREE.desc() : HOST.COMPUTE_FREE.asc()), HOST.ID.asc())
-                .fetchLazy();
+        final Cursor<Record2<Long, Long>> cursor;
+        if(hosts){
+        	
+        	Table<Record4<Long, Long, Long, Integer>> availableHosts=create().
+    		select(HOST.ID,HOST.ACCOUNT_ID,HOST.AGENT_ID,INSTANCE_NUM_PER_HOST).
+    		from(HOST).
+    		leftOuterJoin(INSTANCE_HOST_MAP)
+        		.on(INSTANCE_HOST_MAP.HOST_ID.eq(HOST.ID))
+        	.leftOuterJoin(INSTANCE)
+        		.on(INSTANCE_HOST_MAP.INSTANCE_ID.eq(INSTANCE.ID))
+        	 .where(HOST.STATE.in(CommonStatesConstants.ACTIVE, CommonStatesConstants.UPDATING_ACTIVE))
+        	 .and(INSTANCE_HOST_MAP.STATE.eq(CommonStatesConstants.ACTIVE).or(INSTANCE_HOST_MAP.STATE.eq(CommonStatesConstants.INACTIVE).and(INSTANCE.STATE.eq(InstanceConstants.STATE_STARTING))))
+        	.groupBy(HOST.ID)
+            .asTable("host");
+        	cursor= create()
+                    .select(HOST.ID, STORAGE_POOL.ID)
+                    .from(availableHosts)
+                    .leftOuterJoin(STORAGE_POOL_HOST_MAP)
+                        .on(STORAGE_POOL_HOST_MAP.HOST_ID.eq(HOST.ID)
+                            .and(STORAGE_POOL_HOST_MAP.REMOVED.isNull()))
+                    .join(STORAGE_POOL)
+                        .on(STORAGE_POOL.ID.eq(STORAGE_POOL_HOST_MAP.STORAGE_POOL_ID))
+                    .leftOuterJoin(AGENT)
+                        .on(AGENT.ID.eq(HOST.AGENT_ID))
+                    .where(
+                        AGENT.ID.isNull().or(AGENT.STATE.eq(CommonStatesConstants.ACTIVE))
+                        .and(STORAGE_POOL.STATE.eq(CommonStatesConstants.ACTIVE))
+                        .and(getQueryOptionCondition(options)))
+                    .orderBy(INSTANCE_NUM_PER_HOST.asc())
+                    .fetchLazy();
+        }else{
+        	cursor = create()
+                    .select(HOST.ID, STORAGE_POOL.ID)
+                    .from(HOST)
+                    .leftOuterJoin(STORAGE_POOL_HOST_MAP)
+                        .on(STORAGE_POOL_HOST_MAP.HOST_ID.eq(HOST.ID)
+                            .and(STORAGE_POOL_HOST_MAP.REMOVED.isNull()))
+                    .join(STORAGE_POOL)
+                        .on(STORAGE_POOL.ID.eq(STORAGE_POOL_HOST_MAP.STORAGE_POOL_ID))
+                    .leftOuterJoin(AGENT)
+                        .on(AGENT.ID.eq(HOST.AGENT_ID))
+                    .where(
+                        AGENT.ID.isNull().or(AGENT.STATE.eq(CommonStatesConstants.ACTIVE))
+                        .and(HOST.STATE.in(CommonStatesConstants.ACTIVE, CommonStatesConstants.UPDATING_ACTIVE))
+                        .and(STORAGE_POOL.STATE.eq(CommonStatesConstants.ACTIVE))
+                        .and(getQueryOptionCondition(options)))
+                    .orderBy((SPREAD.get() ? HOST.COMPUTE_FREE.desc() : HOST.COMPUTE_FREE.asc()), HOST.ID.asc())
+                    .fetchLazy();
+
+        }
+        	
 
         return new AllocationCandidateIterator(objectManager, cursor, volumes, hosts, callback);
     }
